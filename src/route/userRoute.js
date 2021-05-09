d099589a4b9e1907341cf356e5722ed3582aeb87
Samuel Osinloye <psalmueloye@gmail.com>
2021-05-09 22:25:20 +0100

    fix route error

--- src/route/userRoute.js
+++ src/route/userRoute.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const userCtrl = require("../controller/userController");
 
 router.post("/users", userCtrl.createUser);
-router.get("/users:id", userCtrl.getUser);
+router.get("/users/:id", userCtrl.getUser);
 router.get("/users", userCtrl.getUsers);
-router.put("/users:id", userCtrl.updateUser);
-router.delete("/users:id", userCtrl.deleteUser);
+router.put("/users/:id", userCtrl.updateUser);
+router.delete("/users/:id", userCtrl.deleteUser);
 
 
 module.exports = router;
